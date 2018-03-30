7d11d4abed037653c95bf62795550657073b3efe
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-03-30 11:09:49 +0530

    added model classes for District

---
+++ app/src/main/java/com/rohantaneja/monitoringvisits/model/District.java
@@ -0,0 +1,44 @@
+package com.rohantaneja.monitoringvisits.model;
+
+import java.util.List;
+
+/**
+ * Created by rohantaneja on 30/03/18.
+ */
+
+public class District {
+
+    private int districtId;
+    private String districtName;
+    private List<DistrictStatistic> districtStatisticList;
+
+    public District(int districtId, String districtName, List<DistrictStatistic> districtStatisticList) {
+        this.districtId = districtId;
+        this.districtName = districtName;
+        this.districtStatisticList = districtStatisticList;
+    }
+
+    public void setDistrictId(int districtId) {
+        this.districtId = districtId;
+    }
+
+    public void setDistrictName(String districtName) {
+        this.districtName = districtName;
+    }
+
+    public void setDistrictStatisticList(List<DistrictStatistic> districtStatisticList) {
+        this.districtStatisticList = districtStatisticList;
+    }
+
+    public int getDistrictId() {
+        return districtId;
+    }
+
+    public String getDistrictName() {
+        return districtName;
+    }
+
+    public List<DistrictStatistic> getDistrictStatisticList() {
+        return districtStatisticList;
+    }
+}
