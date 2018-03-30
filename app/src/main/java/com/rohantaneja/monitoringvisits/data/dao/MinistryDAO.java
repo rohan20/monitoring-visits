0a6d8640cc86810a914bd7dd1a69ec5379a46036
Rohan <rohanraarora@gmail.com>
2018-03-30 15:55:54 +0530

    Add Room and entities

---
+++ app/src/main/java/com/rohantaneja/monitoringvisits/data/dao/MinistryDAO.java
@@ -0,0 +1,57 @@
+package com.rohantaneja.monitoringvisits.data.dao;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+import com.rohantaneja.monitoringvisits.model.Question;
+import com.rohantaneja.monitoringvisits.model.QuestionOption;
+import com.rohantaneja.monitoringvisits.model.Task;
+import com.rohantaneja.monitoringvisits.model.Visit;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by ralph on 30/03/18.
+ */
+
+@Dao
+public interface MinistryDAO {
+
+    @Insert
+    void insertTasks(ArrayList<Task> tasks);
+
+    @Query("SELECT * FROM tasks WHERE districtName = :districtName")
+    List<Task> getTasksForDistrict(String districtName);
+
+    @Query("SELECT * FROM tasks WHERE programmeId = :programmeId")
+    List<Task> getTasksForProgramme(int programmeId);
+
+    @Query("SELECT * FROM visits WHERE taskId = :taskId")
+    List<Visit> getVisitsForTask(int taskId);
+
+    @Insert
+    void insertVisits(ArrayList<Visit> visits);
+
+    @Insert
+    void insertVisit(Visit visit);
+
+    @Query("SELECT * FROM questions WHERE programmeId = :programmeId AND visitType = :visitType")
+    List<Question> getQuestionsForProgrammeAndVisitType(int programmeId, String visitType);
+
+    @Insert
+    void insertQuestions(ArrayList<Question> questions);
+
+    @Insert
+    void insertQuestion(Question questions);
+
+
+    @Query("SELECT * FROM questionOptions WHERE questionId = :questionId")
+    List<QuestionOption> getQuestionOptions(int questionId);
+
+    @Insert
+    void insertQuestionOptions(ArrayList<QuestionOption> questionOptions);
+
+
+}
