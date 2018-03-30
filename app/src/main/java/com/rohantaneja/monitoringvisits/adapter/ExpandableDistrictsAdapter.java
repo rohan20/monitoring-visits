2c8b79fe0582470734caa6cec2f567b73c95ed78
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-03-30 15:25:23 +0530

    added ExpandableDistrictsAdapter

---
+++ app/src/main/java/com/rohantaneja/monitoringvisits/adapter/ExpandableDistrictsAdapter.java
@@ -0,0 +1,62 @@
+package com.rohantaneja.monitoringvisits.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+
+/**
+ * Created by rohantaneja on 30/03/18.
+ */
+
+public class ExpandableDistrictsAdapter extends BaseExpandableListAdapter {
+
+    @Override
+    public int getGroupCount() {
+        return 0;
+    }
+
+    @Override
+    public int getChildrenCount(int i) {
+        return 0;
+    }
+
+    @Override
+    public Object getGroup(int i) {
+        return null;
+    }
+
+    @Override
+    public Object getChild(int i, int i1) {
+        return null;
+    }
+
+    @Override
+    public long getGroupId(int i) {
+        return 0;
+    }
+
+    @Override
+    public long getChildId(int i, int i1) {
+        return 0;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int i, boolean b, View view, ViewGroup viewGroup) {
+        return null;
+    }
+
+    @Override
+    public View getChildView(int i, int i1, boolean b, View view, ViewGroup viewGroup) {
+        return null;
+    }
+
+    @Override
+    public boolean isChildSelectable(int i, int i1) {
+        return false;
+    }
+}
