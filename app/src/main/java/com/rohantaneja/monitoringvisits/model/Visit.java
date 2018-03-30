b93595c4c915ca934cf0aa83f54813825dc2fd33
Rohan <rohanraarora@gmail.com>
2018-03-31 05:09:45 +0530

    Add visits call

--- app/src/main/java/com/rohantaneja/monitoringvisits/model/Visit.java
+++ app/src/main/java/com/rohantaneja/monitoringvisits/model/Visit.java
@@ -1,8 +1,11 @@
 package com.rohantaneja.monitoringvisits.model;
 
+import android.arch.persistence.room.Embedded;
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.ForeignKey;
 import android.arch.persistence.room.PrimaryKey;
+
+import com.google.gson.annotations.SerializedName;
 
 /**
  * Created by ralph on 30/03/18.
@@ -16,9 +19,19 @@ import android.arch.persistence.room.PrimaryKey;
 public class Visit {
 
     @PrimaryKey
+    @SerializedName("vid")
     private int id;
+    @SerializedName("tid")
     private int taskId;
-    private String action;
+    @SerializedName("remarkOfficer")
+    private String officerRemark;
+    @SerializedName("remarkAdmin")
+    private String adminRemark;
+    private Double lat;
+    private Double longitude;
+    @Embedded
+    @SerializedName("actionId")
+    private VisitAction action;
 
     public int getId() {
         return id;
@@ -36,11 +49,33 @@ public class Visit {
         this.taskId = taskId;
     }
 
-    public String getAction() {
+    public VisitAction getAction() {
         return action;
     }
 
-    public void setAction(String action) {
+    public void setAction(VisitAction action) {
         this.action = action;
+    }
+
+    static class VisitAction {
+
+        private int actionId;
+        private String action;
+
+        public int getActionId() {
+            return actionId;
+        }
+
+        public void setActionId(int actionId) {
+            this.actionId = actionId;
+        }
+
+        public String getAction() {
+            return action;
+        }
+
+        public void setAction(String action) {
+            this.action = action;
+        }
     }
 }
---
+++ app/src/main/java/com/rohantaneja/monitoringvisits/network/response/VisitsResponse.java
@@ -0,0 +1,14 @@
+package com.rohantaneja.monitoringvisits.network.response;
+
+import com.rohantaneja.monitoringvisits.model.Visit;
+
+import java.util.ArrayList;
+
+/**
+ * Created by ralph on 31/03/18.
+ */
+
+public class VisitsResponse {
+
+    public ArrayList<Visit> visitList;
+}
