864bce43b061b7f2b51e0c4c4a7f20c29a6efa44
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-03-30 23:04:33 +0530

    added VisitActivity and corresponding TODOs

---
+++ app/src/main/java/com/rohantaneja/monitoringvisits/ui/VisitActivity.java
@@ -0,0 +1,21 @@
+package com.rohantaneja.monitoringvisits.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.rohantaneja.monitoringvisits.BaseActivity;
+import com.rohantaneja.monitoringvisits.R;
+
+public class VisitActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visit);
+
+        //TODO ask for location -> show empty view if no location, else show fields to add visit
+        //TODO buttons to start questionnaire or cancel visit
+        //TODO programmatically display ques
+        //TODO check for internet while saving, if no internet, save to local
+    }
+}
